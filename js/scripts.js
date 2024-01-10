d63546bd0a1d4740f4b8e4812b7f41d91cd867b8
MirkoCifelli <ciffo4@hotmail.it>
2024-01-10 14:27:10 +0100

    Completato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,14 @@ createApp({
         return {
             emails :[]
         };
+    },
+    mounted(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) =>{
+                this.emails.push(res.data.response);
+            })
+            
+        }
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
